f0e36e6b2b64d5739036c75c4f4b49dc8d7588c4
Sourabh R <anon3mou5@Sourabhs-MacBook-Pro.local>
2025-02-21 06:26:12 +0530

    update config.py to load env vars

--- configuration/config.py
+++ configuration/config.py
@@ -1,7 +1,9 @@
 # Constants
-DATABASE_URL = "db url"
-ASYNC_DATABASE_URL = "db url"
+import os
+
+DATABASE_URL = os.environ.get("DATABASE_URL")
+ASYNC_DATABASE_URL = os.environ.get("DATABASE_URL")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 MAX_BOOKINGS = 2
-SECRET_KEY = "secret key"
+SECRET_KEY = os.environ.get("SECRET_KEY")
